78f6ee0748ddaa98da78ddb9286f0a930bf38f6d
Phillipe Augusto <philliipe@msn.com>
2024-04-25 06:57:35 -0300

    Ajustes Arquivos.

--- MotokaEasy.Core/ConstantsApp/QueueMessageBrokerConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MotokaEasy.Core.ConstantsApp;
-
-public static class QueueMessageBrokerConstants
-{
-    private const string Queue = ".queue-";
-    public static readonly string QueueExtratorDeDados = ApplicationConstants.NameApplication + Queue + "extrator_dados_operacao";
-    public static readonly string QueueExtratorDeDadosErro = ApplicationConstants.NameApplication + Queue + "extrator_dados_operacao_error";
-
-}
